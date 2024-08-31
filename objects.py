ee376d6a18e90e0275643313ea2d64a1e4564b58
vader066 <humphreydonkor066@gmail.com>
2024-08-31 20:18:34 +0000

    all functions for calculating  mismatch vector written

--- objects.py
+++ objects.py
@@ -1,5 +1,10 @@
+import numpy as np
 import bisect
 
+
+
+
+#Class for the various quantites of interest for Load flow studies [P, Q, V, D]
 class Qty:
   def __init__(self, type, bus, value ):
     self.bus = bus
@@ -42,13 +47,14 @@ class Qty:
     
     
 
-# Vector of Know Quantities
+
+
+# Class for the Vector of Known Quantities
 class Kvector:
   def __init__(self):
     self.data = []
     
-  #adds quantities to the known vector but ensures they are only of type P and Q
-  #and arranges them in the right order
+  #adds quantities objects to the vector but ensures they are only of type P and Q and arranges them in the right order
   def push(self, qty):
     types = ['P', 'Q']
     if not isinstance(qty, Qty):
@@ -56,30 +62,32 @@ class Kvector:
     if qty.type.upper() not in types:
       raise TypeError("cannot add V or D quantities to Vector of Knowns")
     
-    # sort Vector so that P's come before Q's and they are arranged in ascending order
+    # Rest of the code below is for sorting Vector so that P's come before Q's and they are arranged in ascending order of bus numbers
+    
     type_order = {'P': 0, 'Q': 1}
     new_entry = (type_order[qty.type], qty.bus)
         
-    # Use bisect to find the correct insertion index
+    # Using bisect to find the correct insertion index
     sortable_entries = [(type_order[obj.type], obj.bus) for obj in self.data]
     index = bisect.bisect_left(sortable_entries, new_entry)
     self.data.insert(index, qty)
     
     
-  #for displaying the array in a user-friendly way in the console
+  #for displaying the array in a user-friendly way in the console when you print objects of the class
   def __repr__(self):
     return repr([f"{qty.type}{qty.bus}: {qty.value}" for qty in self.data])
     
     
+
+
     
     
-# Vector of Know Quantities
+# Class for the Vector of Unknown Quantities
 class Uvector:
   def __init__(self):
     self.data = []
     
-  #adds quantities to the unknown vector but ensures they are only of type V and D
-  #and arranges them in the right order
+  #adds quantities to the unknown vector but ensures they are only of type V and D and arranges them in the right order
   def push(self, qty):
     types = ['V', 'D']
     if not isinstance(qty, Qty):
@@ -87,11 +95,11 @@ class Uvector:
     if qty.type.upper() not in types:
       raise TypeError("cannot add P or Q quantities to Vector of Unknowns")
     
-    # sort Vector so that D's come before V's and they are arranged in ascending order
+    # For sorting the Vector so that D's come before V's and they are arranged in ascending order of bus numbers
     type_order = {'D': 0, 'V': 1}
     new_entry = (type_order[qty.type], qty.bus)
         
-    # Use bisect to find the correct insertion index
+    # Using bisect to find the correct insertion index
     sortable_entries = [(type_order[obj.type], obj.bus) for obj in self.data]
     index = bisect.bisect_left(sortable_entries, new_entry)
     self.data.insert(index, qty)
@@ -101,28 +109,109 @@ class Uvector:
   def __repr__(self):
     return repr([f"{qty.type}{qty.bus}: {qty.value}" for qty in self.data])
     
+
+
+
+ 
+#Function to calculate P. Takes arguments of the bus, delta vector, Voltage vector and Addmittance matrix
+def calc_P_i(bus, D, V, Y):
+  i = bus-1  # Index for P_i
+  
+  #Checks arrays to ensure they are of matching order
+  if D.shape != V.shape:
+    raise TypeError("V matrix and D matrix must have the same order")
+  elif len(Y) != len(V) and len(Y[0] != len(V)):
+    raise TypeError(f"Admittance matrix must be a {len(V)} by {len(V)} matrix")
+
+  sum_term = 0
+  for j in range(len(V)):
+    #converting angles from delta array into radians
+    Dj_rad = np.radians(D[j])
+    Di_rad = np.radians(D[i])
     
-v2 = Qty("v", 2, 1.32)    
-v1 = Qty("v", 1, 1.05)
-v3 = Qty("v", 3, 1.32)    
-d2 = Qty("d", 2, 1.32)    
-v4 = Qty("v", 4, 1.32)    
-d3 = Qty("d", 3, 1.32)    
+    #computing summation terms first
+    cos_term = np.cos(np.angle(Y[i, j]) + Di_rad - Dj_rad)   #this works even though it should be Dj_rad - Di_rad
+    sum_term += V[j] * np.abs(Y[i, j]) * cos_term
+
+  P_i = V[i] * sum_term   #multiplying summation term with V
+
+  # print(f"P[{bus}]:", P_i)
+  
+  return P_i
+  
+
+
+
+  
+  
+#Function to calculate Q. Takes the same arguments as for P above
+def calc_Q_i(bus, D, V, Y):
+  i = bus-1  # Index for Q_i
+  
+  #Checks arrays to ensure they are of matching order
+  if D.shape != V.shape:
+    raise TypeError("V matrix and D matrix must have the same order")
+  elif len(Y) != len(V) and len(Y[0] != len(V)):
+    raise TypeError(f"Admittance matrix must be a {len(V)} by {len(V)} matrix")
+
+  sum_term = 0
+  for j in range(len(V)):
+    #converting angles from delta array into radians
+    Dj_rad = np.radians(D[j])
+    Di_rad = np.radians(D[i])
     
-Vector_of_Knowns = Uvector()
+    #computing summation terms first
+    cos_term = np.sin(np.angle(Y[i, j]) + Di_rad - Dj_rad)   #this works even though it should be Dj_rad - Di_rad
+    sum_term += V[j] * np.abs(Y[i, j]) * cos_term
 
-Vector_of_Knowns.push(v3)
-Vector_of_Knowns.push(d3)
-Vector_of_Knowns.push(v2)
-Vector_of_Knowns.push(v4)
-Vector_of_Knowns.push(v1)
-Vector_of_Knowns.push(d2)
+  Q_i = -V[i] * sum_term   #multiplying summation term with V
 
-print(Vector_of_Knowns)
-
+  # print(f"Q[{bus}]:", Q_i)
+  
+  return Q_i
 
 
+
+
+
+#Function for calculating the Mismatch vector. 
+# Takes in arguments: 
+# Vector of specified values:
+# delta vector for angles of V
+# Voltage vector
+# Admittance Matrix
+
+# NOTE!!!: The vector of specified values MUST not be a normal array. It should be an
+#instance of the Kvector class 'filled' with objects of the specified values
+
+def MismatchV(spec, D, V, Y):
+  # Initialized empty Vector for calculated Values of P and Q's 
+  calc = Kvector()
+
+  #Calculating for Quantities corresponding to the same quantities in the specified vector
+  for qty in spec.data:
+    if qty.type == "P":
+      val = calc_P_i(qty.bus, D, V, Y)
+    elif qty.type == "Q":
+      val =calc_Q_i(qty.bus, D, V, Y)
+      
+    entry = Qty(qty.type, qty.bus, val)
+    calc.push(entry) 
+
+  #NOTE!! This function returns the kVector object, mismatch, initialized below  
+  mismatch = Kvector()
+
+  #Calculating differences between specified data and calculated data
+  order = len(calc.data)    
+  for i in range(order):
+    qty_cal = calc.data[i]
+    qty_spec = spec.data[i]
+    calc_val = qty_spec.value - qty_cal.value
+    entry = Qty(qty_cal.type, qty_cal.bus, calc_val)
+    mismatch.push(entry)
     
+  print(mismatch)
+  return mismatch
   
     
     
