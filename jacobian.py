94fb43581005922e436c82e1f0a24090ff9a1d45
vader066 <humphreydonkor066@gmail.com>
2024-09-01 19:19:23 +0000

    functions have successfully been dynamically generated

--- jacobian.py
+++ jacobian.py
@@ -14,25 +14,27 @@ import numpy as np
 def Jacobian(Kvector, Y):
   for obj in Kvector.data:
     i = obj.bus
-    j_indices = range(len(Kvector.data))
     
-    # Dynamically defining variables
+    #Generating the buses numbers for variables in the equation of each Known Value
+    j_indices = [idx + 1 for idx in range(len(Kvector.data)) ]
+  
+    
+    # Dynamically defining variables of corresponding buses
     Vi = sp.symbols(f"V{i}")
     Vj = [sp.symbols(f"V{j}") for j in j_indices]
     Di = sp.symbols(f"D{i}")
     Dj = [sp.symbols(f"D{j}") for j in j_indices]
-    print(Vi)
-    print(Vj)
-    print(Di)
-    print(Dj)
     
     def generate_function(Vi, Vj, Di, Dj):
-      summation_term = sum(Vj[k] * Y[i, k] * sp.cos(Dj[k] - Di) for k in range(len(Vj)))
-      return Vi * summation_term
+      summation_term = sum(Vj[k] * np.abs(Y[i-1, k]) * sp.cos(np.angle(Y[i-1, k]) + Dj[k] - Di) for k in range(len(Vj)))
+      full = Vi * summation_term
+      print(full)
+      return full
       
     
     f = generate_function(Vi, Vj, Di, Dj)
     diff = sp.diff(f, Vj[0] )
+    print(diff)
   
   return diff
 
@@ -50,5 +52,5 @@ Y_matrix = np.array([[rect(24.23, -75.95), rect(12.13, 104.04), rect(12.13, 104.
                      [rect(12.13, 104.04), rect(12.13, 104.04), rect(24.23, -75.95)]],
                     dtype=np.complex64)
 
-
+print(np.abs(Y_matrix[0, 0]))
 Jacobian(specified, Y_matrix)
